1728aee93cd99fa6ab1ea310bb01adbf09d40765
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-07-05 15:33:16 +0200

    Add "Class must be instantiated!" error

--- classify128.js
+++ classify128.js
@@ -6,27 +6,71 @@ const Classify = function (pClass = {})
 {
     const _class = function ()
     {
+        if (this.constructor !== _class)
+            throw new Error("Class must be instantiated!");
+        
         const _this = this;
         
-        const args = [
-            function ()
-            {
-                (pClass.extends || Object).apply(_this, arguments);
-                Object.assign(_this, _class.prototype);
-            }
-        ];
+        const args = [ __super.bind(_this) ];
         
         Array.prototype.forEach.call(arguments, a => args.push(a));
         
-        (pClass.constructor || function (_super) { _super(); }).apply(this, args);
+        __class.constructor.apply(this, args);
     };
     
-    _class.prototype = Object.create((pClass.extends || { prototype: {} }).prototype);
+    const __class = {
+        constructor: pClass.constructor || function (_super)
+        {
+            _super();
+        },
+        
+        extends: pClass.extends || Object,
+        
+        prototype: pClass.prototype || {},
+        
+        static: (function ()
+        {
+            const pStatic = pClass.static || {};
+            const _static = {};
+            for (const i in pStatic)
+            {
+                if (i !== "_class" && i !== "_super")
+                    _static[i] = pStatic[i];
+            }
+            return _static;
+        })()
+    };
+    
+    const __super = function ()
+    {
+        if (typeof __class.extends._class === "undefined")
+            __class.extends.apply(this, arguments);
+        else
+        {
+            const _this = this;
+            
+            const args = [ __class.extends._super.bind(_this) ];
+            
+            Array.prototype.forEach.call(arguments, a => args.push(a));
+            
+            __class.extends._class.constructor.apply(this, args);
+        }
+        
+        Object.assign(this, _class.prototype);
+    };
+    
+    _class.prototype = Object.create(__class.extends.prototype);
+    
     _class.prototype.constructor = _class;
     
-    Object.assign(_class.prototype, pClass.prototype || {});
+    Object.assign(_class.prototype, __class.prototype);
     
-    Object.assign(_class, pClass.static || {});
+    Object.assign(_class, __class.static);
+    
+    Object.assign(_class, {
+        _class: __class,
+        _super: __super
+    });
     
     return _class;
 };
